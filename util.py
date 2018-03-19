4e301f194c53d682318519c04b5cc98be74b3804
mrlsdvd <mrlsdvd@DN0a1fcb65.SUNet>
2018-03-19 00:16:39 -0700

    Add signal plotting to util

--- util.py
+++ util.py
@@ -1,7 +1,11 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
+import config
 from scipy.io.wavfile import read, write
 from pydub import AudioSegment
+import librosa
+import librosa.display
 import os
 import datetime
 
@@ -17,6 +21,25 @@ def decode(filename):
     as_array = np.array(sound.get_array_of_samples())
     return convert_to_wav(as_array)
 
+def plot_track_signal(filename, outname="plot.png"):
+    plt.rcParams['figure.figsize'] = (17, 5)
+    plt.figure()
+    x, sr = librosa.load(filename, sr=None, mono=True)
+    librosa.display.waveplot(x, sr, alpha=0.5);
+    plt.savefig(outname)
+    plt.clf()
+
+    # vlines = [5, 10, 15, 20, 25]
+    # librosa.display.waveplot(x, sr, alpha=0.5);
+    # plt.vlines(vlines, -1, 1)
+    # plt.savefig("plot_full_vlines.png")
+    # plt.clf()
+    #
+    # start, end = 8000, 5
+    # plt.plot(x[start:start+2000])
+    # plt.ylim((-1, 1));
+    # plt.savefig("plot_snippet.png")
+
 def make_dataset():
     dataset = []
     for directory in os.listdir('fma_small'):
@@ -30,8 +53,34 @@ def make_dataset():
                     except Exception as e:
                         continue
                     print song
-            np.save('dataset/' + directory, np.stack(dataset))
+            np.save(os.path.join(config.data_dir, directory), np.stack(dataset))
             print np.stack(dataset).shape
+            dataset = []
+        except ValueError:
+            # Skip irrelevant directories (Looking at you .DS_Store)
+            continue
+
+def make_disjoint_dataset():
+    """
+    Writes each track into 15 separate 2-second files.
+    To make loading and training on batches more memory efficient
+    """
+    for directory in os.listdir('fma_small'):
+        try:
+            if int(directory) <= 28:
+                continue
+            for song in os.listdir('fma_small/' + directory):
+                dataset = []
+                if song.endswith('.mp3'):
+                    try:
+                        dataset += np.split(decode('fma_small/'+directory+'/'+song)[:1320000], 6)
+                    except Exception as e:
+                        continue
+                    print song
+                for i, chunk in enumerate(dataset):
+                    chunk_name = "{}_{}_{}".format(directory, song.split('.')[0], i)
+                    np.save(os.path.join(config.data_dir, chunk_name), chunk)
+            # print np.stack(dataset).shape
             dataset = []
         except ValueError:
             # Skip irrelevant directories (Looking at you .DS_Store)
